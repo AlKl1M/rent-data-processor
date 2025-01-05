f8a72dc75a2b1d8b44df0be2bb0d7e54cc7bcb52
Александр Климковецкий <al.klimkovetskiy@gmail.com>
2025-01-05 15:18:01 +0200

    chore: Initial commit

---
+++ src/test/java/com/alkl1m/rentdataprocessor/RentDataProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkl1m.rentdataprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentDataProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
