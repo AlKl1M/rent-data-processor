a71cdc099c42a3db9454aa616ba59ee63a4ca56a
Александр Климковецкий <al.klimkovetskiy@gmail.com>
2025-01-06 10:55:47 +0200

    feat: использование ReentrantLock в ThreadSafeMultiResourceReader

---
+++ src/main/java/com/alkl1m/rentdataprocessor/reader/ThreadSafeMultiResourceReader.java
@@ -0,0 +1,58 @@
+package com.alkl1m.rentdataprocessor.reader;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.core.io.Resource;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreadSafeMultiResourceReader<T> implements ItemReader<T> {
+
+    private final MultiResourceItemReader<T> delegateReader;
+    private final Lock reentrantLock = new ReentrantLock();
+
+    public ThreadSafeMultiResourceReader(MultiResourceItemReader<T> delegateReader) {
+        this.delegateReader = delegateReader;
+    }
+
+    @Override
+    public T read() throws Exception, UnexpectedInputException, ParseException {
+        reentrantLock.lock();
+        try {
+            return delegateReader.read();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void setResources(Resource[] resources) {
+        reentrantLock.lock();
+        try {
+            delegateReader.setResources(resources);
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void open(ExecutionContext executionContext) {
+        reentrantLock.lock();
+        try {
+            delegateReader.open(executionContext);
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public void close() {
+        reentrantLock.lock();
+        try {
+            delegateReader.close();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+}
